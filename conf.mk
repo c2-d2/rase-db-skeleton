d268bbddb5644108b5fc7d940e3f496bae702d9b
Karel Břinda <karel.brinda@gmail.com>
2019-02-13 17:02:08 -0500

    Add skeleton files

---
+++ conf.mk
@@ -0,0 +1,22 @@
+#########
+# INDEX #
+#########
+
+#db name
+name=my-db
+
+# k-mer size, can contain multiple values
+k=18
+
+###############
+# BREAKPOINTS #
+###############
+
+ant1=0.06  # Antibiotics 1
+ant2=0.25  # Antibiotics 2
+
+############
+# PLOTTING #
+############
+antibiotics=ant1 ant2
+
