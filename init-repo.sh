55c4c1cf1ed93f6bdc176449c3d18cd734892f10
Karel Břinda <karel.brinda@gmail.com>
2019-03-07 15:06:33 -0500

    Add init-repo.sh

---
+++ init-repo.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+set -e
+set -o pipefail
+set -u
+
+sha=$(curl -L https://api.github.com/repos/c2-d2/rase-db-skeleton/contents/rase | python -c "import sys, json; print json.load(sys.stdin)['sha']")
+
+curl -L https://github.com/c2-d2/rase-db-skeleton/archive/master.tar.gz | tar xvf - --strip-components 1
+
+git init
+git submodule add ssh://git@github.com/karel-brinda/rase
+(cd rase && git checkout $sha)
+
