de947afe29c8f7cc20d16d14fafca9c2119ef032
Karel Břinda <karel.brinda@gmail.com>
2019-04-01 09:53:29 -0400

    Split configuration and init makefiles

--- conf.mk
+++ conf.mk
@@ -35,23 +35,3 @@ antibiotics:=ant1 ant2
 ant1:=0.06  # Antibiotics 1
 ant2:=0.50  # Antibiotics 2
 
-
-#############
-# INIT MAKE #
-#############
-
-SHELL=/usr/bin/env bash -eo pipefail
-export PATH := ../../scripts:../rase/src/rase:$(PATH)
-
-.SECONDARY:
-.SUFFIXES:
-.PHONY: all clean cleanall help
-
-all:
-
-help: ## Print help message
-	@echo "$$(grep -hE '^\S+:.*##' $(MAKEFILE_LIST) | sed -e 's/:.*##\s*/:/' -e 's/^\(.\+\):\(.*\)/\\x1b[36m\1\\x1b[m:\2/' | column -c2 -t -s : | sort)"
-
-clean: ## Clean
-
-cleanall: clean ## Clean all
